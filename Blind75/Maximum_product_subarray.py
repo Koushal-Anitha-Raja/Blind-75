e27f13e7eea1ecce7fbf8f3a41edb046448600ed
koushal AR <95262944+Koushal-Anitha-Raja@users.noreply.github.com>
2023-03-18 18:21:44 -0400

    Update Maximum_product_subarray.py

--- Blind75/Maximum_product_subarray.py
+++ Blind75/Maximum_product_subarray.py
@@ -1,39 +1,59 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-
+        #curr min variable to nums[0]
         currentMinimum = nums[0]
+        #curr max variable to nums[0]
         currentMaximum = nums[0]
+        #global max var to nums[0]
         globalMaximum = nums[0]
 
+        #start and end var to zero and one values respectively
         start = 0
         end = 1
         
+        #iterate through the nums 
         for i in range(1,len(nums)):
+            #Returning the maximum between currmax and curr min by having currmax value =currmin*current nums,currmax*nums[i],current nums and min between  currmin*current nums,currmax*nums[i],current nums
             currentMaximum,currentMinimum = max(currentMinimum * nums[i], currentMaximum * nums[i], nums[i]), min(currentMinimum * nums[i], currentMaximum * nums[i], nums[i])
               
+                #if currmax is greater than currmin
             if (currentMaximum > globalMaximum):
+                #assign globalmax to currentmax
                 globalMaximum = currentMaximum
+                #increment end value by one
                 end=i+1
+                #assign i value to j
                 j=i
+                #assign xurrentmax to contributor
                 contributer = currentMaximum
+                #iterate until j is greater than 0
                 while j>=0:
+                    #if nums[j] equal to zero
                     if nums[j]==0:
+                        #assign start to j
                         start = j
+                        #break the state
                         break
+                    #or else if it is equal or lesser    
                     else:
                         if contributer == nums[j]:
                             start = j
+                            #assign start to j and break
                             break
+                            #contributor is contributor dived by nums[j]
                         contributer = contributer/nums[j]
+                    #decrement j by one
                     j-=1
 
 
-    
+    #assign prod to one
         prod = 1
+        #iterate through the nums from start to end
         for num in nums[start: end]:
+            #prod =prod *num
             prod *=num
             print(num)
-
+#return the prod
         return prod
      
 
