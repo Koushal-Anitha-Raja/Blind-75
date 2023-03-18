ddbdd6987750d5f6fe682c0ba3c6e5a7d9645389
koushal AR <95262944+Koushal-Anitha-Raja@users.noreply.github.com>
2023-03-18 16:41:09 -0400

    Update Buy_and_sell_stock.py

--- Blind75/Buy_and_sell_stock.py
+++ Blind75/Buy_and_sell_stock.py
@@ -1,16 +1,24 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        #assign left and right pointers to zero and first position
         l,r=0,1
+        #assign maxprofit as zero 
         maxprofit=0
+        #iterate through until right pointer is less than lenght of prices
         while r<len(prices):
             #profitable
             if prices[r]>prices[l]:
+                #assign profit variable as prices of right minus left pointers
                 profit=prices[r]-prices[l]
+                #compare maxprofit with maxprofit and profit and assign it to maxprofit
                 maxprofit=max(maxprofit,profit)
+            #if not profitable
             else:
+                #left pointer equals to right
                 l=r
+            #increment right to right plus one
             r=r+1    
-            
+         #return the maxprofit    
         return maxprofit    
                 
                 
