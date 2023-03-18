691d20cedb186d5a5523073ef04ecabb06b0e812
koushal AR <95262944+Koushal-Anitha-Raja@users.noreply.github.com>
2023-03-18 18:30:04 -0400

    Update Twosum.py

--- Blind75/Twosum.py
+++ Blind75/Twosum.py
@@ -3,12 +3,17 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         hashmap={}
         #key is n and value is index
-        
+        #iterate through the i and n values 
         for i ,n in enumerate(nums):
+            #difference value is target minus n value
             diff=target-n
+            #if difference in hashmap
             if diff in hashmap:
+                #return the hashmap value od the diff var and current i th nums
                 return [hashmap[diff],i]
+            #assign hashmap of n to i
             hashmap[n]=i
+            
         return     
         
         
