2c87acddaf139160d7a47e5116c975e56354c330
koushal AR <95262944+Koushal-Anitha-Raja@users.noreply.github.com>
2023-03-18 17:45:02 -0400

    Update Containsduplicate.py

--- Blind75/Containsduplicate.py
+++ Blind75/Containsduplicate.py
@@ -1,11 +1,15 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
+        #lenght of the a
         a=len(nums)
+        #add the b value to hashset to remove duplicate
         b=len(set(nums))
         print(a)
         print(b)
+        #if it has duplicate return false
         if a==b:
             return False
+        #If it does not have duplicates return true
         else:
             return True
             
