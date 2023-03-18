3aa92a4d409c0a795c3705e966f3248be6f27231
koushal AR <95262944+Koushal-Anitha-Raja@users.noreply.github.com>
2023-03-18 17:43:29 -0400

    Update Climbing_stairs.py

--- Blind75/Climbing_stairs.py
+++ Blind75/Climbing_stairs.py
@@ -1,10 +1,16 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
+        #assign two variables as initial value one
         one,two=1,1
+        #iterate through the n value
         for i in range (n-1):
+            #assigning temp to one for not losing track of the one
             temp=one
+            #adding at each step to one as one plus two
             one=one+two
+            #then assign two to temp variuable
             two = temp
+        #finally return the one 
         return one    
             
             
