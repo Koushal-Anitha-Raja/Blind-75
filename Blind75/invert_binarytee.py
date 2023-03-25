e25a6f6c53d952d3679da2829ce53076a70739a9
Ar Koushal <arkoushal1999@gmail.com>
2023-03-25 14:59:45 -0400

    tree based questions using recursive method

---
+++ Blind75/invert_binarytee.py
@@ -0,0 +1,16 @@
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        #if the root node is not present
+        if not root:
+            return 
+        self.recur(root)
+        return root
+    #recursive method of root     
+    def recur(self,node):
+        if node == None:
+            return 
+        #either you can do pre order or post order but not inorder because the root can be changed and inverted or inverted and roots can be changed
+        self.recur(node.left)
+        self.recur(node.right)
+        # postorder
+        node.left,node.right=node.right,node.left
